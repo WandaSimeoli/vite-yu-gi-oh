fa8f821b0bae986f8de8f86b5f92039a8df6409c
WandaSimeoli <128651503+WandaSimeoli@users.noreply.github.com>
2023-07-17 16:06:37 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+// my-component.js
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components :{
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+  },
+
+  data() {
+    return {  }
+  },
+  methods: {}
+}
+</script>
+
+<template>
+  <HeaderComponent />
+  <MainComponent />
+  <FooterComponent />
+</template>
+
+<style lang="scss">
+@use "assets/scss/main.scss" as *;
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  components :{
+  },
+
+  data() {
+    return {  }
+  },
+  methods: {}
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use "assets/scss/main.scss" as *;
+</style>
